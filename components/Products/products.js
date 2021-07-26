a4d113a01520bdbafc8b170c2f556c951129f13f
Your Name <you@example.com>
2021-07-26 09:43:30 -0700

    addded new files and some folder

---
+++ components/Products/products.js
@@ -0,0 +1,132 @@
+///render otobrazit dannye na strsnichke
+
+class Products {
+   constructor(){
+     this.classNameActive = 'products-element__btn_active';
+     this.labelAdd = 'add in a bag '
+     this.labelRemove = 'delete in a bag '
+   }
+handleSetlocationStorage(element,id){
+ const {pushProduct,products} = localStorageUtil.putProducts(id);
+
+if (pushProduct) {
+  element.classList.add(this.classNameActive)
+  element.innerHTML = this.labelRemove
+}else{
+  element.classList.remove(this.classNameActive)
+  element.innerHTML = this.labelAdd;
+  
+}
+
+headerPage.render( products.length);
+
+}
+
+    render(){
+      const productsStore = localStorageUtil.getProducts()
+        let htmlCatalog = '';
+        CATALOG.forEach(({id,name,price,img })=>{
+          let activeClass = '';
+          let activeText = '';
+
+         if (productsStore.indexOf(id)=== -1){
+           activeText = this.labelAdd;
+         }else{
+          activeClass =' ' +this.classNameActive;
+           activeText = this.labelRemove;
+         }
+  htmlCatalog += `
+  <li  class="products-element">
+<span class="products-element__name">      ${name}</span>
+<img    class="products-element__img" src="${img}"/>
+<span class="products-element__price">
+🤖 ${price.toLocaleString()} USD
+</span>
+<button class="products-element__btn${activeClass}" onClick="productsPage.handleSetlocationStorage(this,'${id}');">
+${activeText}
+</button>
+  </li>
+  `
+        });
+        const html = `
+        <ul class="products-container">
+        ${htmlCatalog}
+        </ul>
+        `;
+
+ROOT_PRODUCTS.innerHTML = html;
+
+
+    }
+}
+
+const productsPage = new Products();
+productsPage.render();
+
+
+
+
+
+
+
+
+// class Products {
+//   constructor() {
+//       this.classNameActive = 'products-element__btn_active';
+//       this.labelAdd = 'Добавить в корзину';
+//       this.labelRemove = 'Удалить из корзины';
+//   }
+
+//   handleSetLocationStorage(element, id) {
+//       const { pushProduct, products } = localStorageUtil.putProducts(id);
+      
+//       if (pushProduct) {
+//           element.classList.add(this.classNameActive);
+//           element.innerHTML = this.labelRemove;
+//       } else {
+//           element.classList.remove(this.classNameActive);
+//           element.innerHTML = this.labelAdd;
+//       }
+//   }
+
+//   render() {
+//       const productsStore = localStorageUtil.getProducts();
+//       let htmlCatalog = '';
+
+//       CATALOG.forEach(({ id, name, price, img }) => {
+//           let activeClass = '';
+//           let activeText = '';
+
+//           if (productsStore.indexOf(id) === -1) {
+//               activeText = this.labelAdd;
+//           } else {
+//               activeClass = ' '+this.classNameActive;
+//               activeText = this.labelRemove;
+//           }
+
+//           htmlCatalog += `
+//               <li class="products-element">
+//                   <span class="products-element__name">${name}</span>
+//                   <img class="products-element__img" src="${img}" />
+//                   <span class="products-element__price">
+//                       ⚡️ ${price.toLocaleString()} USD
+//                   </span>
+//                   <button class="products-element__btn${activeClass}" onclick="productsPage.handleSetLocationStorage(this, '${id}');">
+//                       ${activeText}
+//                   </button>
+//               </li>
+//           `;
+//       });
+
+//       const html = `
+//           <ul class="products-container">
+//               ${htmlCatalog}
+//           </ul>
+//       `;
+
+//       ROOT_PRODUCTS.innerHTML = html;
+//   }
+// }
+
+// const productsPage = new Products();
+// productsPage.render();
---
+++ constans/catalog.js
@@ -0,0 +1,40 @@
+const CATALOG =[
+    {
+        id: 'el1',
+        name: 'Yamaha C40M Гитара классическая.',
+        img:'https://guitar-guitar.ru/upload/resize_cache/iblock/043/500_500_0/vzkwnl3bimerl5up46666q7vnolzzvrs.jpg',
+        price:20000,
+    },
+    {
+        id: 'el2',
+        name: 'new brand is my hand',
+        img:'https://guitar-guitar.ru/upload/resize_cache/iblock/fec/500_500_0/orhpzfaj68tphc7w4q77nosx84pilkqc.jpg',
+        price:12000,
+    },
+    {
+        id: 'el3',
+        name: 'new guitar with my brend',
+        img:'https://guitar-guitar.ru/upload/resize_cache/iblock/cf9/500_500_0/hq1sak1bk8cdpjpjlmhcawl2oeds98q1.jpg',
+        price:3000,
+    },
+
+
+    {
+        id: 'el4',
+        name: 'Yamaha C40M Гитара классическая.',
+        img:'https://guitar-guitar.ru/upload/resize_cache/iblock/043/500_500_0/vzkwnl3bimerl5up46666q7vnolzzvrs.jpg',
+        price:20000,
+    },
+    {
+        id: 'el5',
+        name: 'new brand is my hand',
+        img:'https://guitar-guitar.ru/upload/resize_cache/iblock/b03/500_500_0/kej4mpwngnrpwzp7fxevkozphe71fdl0.jpeg',
+        price:12000,
+    },
+    {
+        id: 'el6',
+        name: 'new guitar with my brend',
+        img:'https://guitar-guitar.ru/upload/resize_cache/iblock/cf9/500_500_0/hq1sak1bk8cdpjpjlmhcawl2oeds98q1.jpg',
+        price:3000,
+    },
+]
